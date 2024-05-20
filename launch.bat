d51b02c7ed089eee809403360306600c0d0d4965
awei55 <141664408+awei55@users.noreply.github.com>
2024-05-20 13:50:17 +0530

    BOTWCEMU
    Run BoTW with CEMU and vJoy without opening either

---
+++ launch.bat
@@ -0,0 +1,3 @@
+@echo off
+start "" "D:\Breath Of The Wild\mouse2joystick_custom_CEMU\mouse2joystick_Custom_CEMU.exe"
+start "" "D:\Breath Of The Wild\CEMU\Cemu.exe" -g "D:\Breath Of The Wild\BOTW\Game\The Legend of Zelda Breath of the Wild [ALZE01]\code\U-King.rpx" -f
